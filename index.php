d49506a04ad1f1602794b3b3bed48aa440da01da
ThinvvGCD18359 <thinvvgcd18359@fpt.edu.vn>
2020-06-19 17:07:17 +0700

    update 2

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <html>
-    <a href="DatabaseIndex.php" target="_blank" >Work with database</a>
-     <br/> 
+    <head>
+        <meta charset="UTF-8">
+        <title>ATN Toy Store</title>
+        <h1> Welcome to ATN Store <h1/>
+    </head>
+    <body>
+        <?php
+        // put your code here
+        ?>
+    </body>
     <a href="ConnectToDB.php" target="_blank" >Connect database</a>
      <br/>
-     <a href="InsertData.php" target="_blank" >Insert data to the database</a>
+    <a href="InsertData.php" target="_blank" >Insert data to the database</a>
      <br/>
-     <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
-    <br/>
     <a href="DeleteData.php" target="_blank" >Delete data to the database</a> 
      <br/>
-     
-
-</body>
-
-     
+    <a href="UpdateData.php" target="_blank" >Update data to the database</a> 
+    <br/>
 </html>
